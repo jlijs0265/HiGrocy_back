8410a5f34408f4d0f4e4ef288a6582fa619e01cc
PARKMINJUN <junee0530@naver.com>
2023-09-06 09:17:49 +0900

    [ADD] OrderDetailContorller + OrederDetailServiceImpl

--- src/main/java/com/example/springreact/controller/OrderDetailController.java
+++ src/main/java/com/example/springreact/controller/OrderDetailController.java
@@ -1,5 +1,18 @@
 package com.example.springreact.controller;
 
+import com.example.springreact.service.OrderDetailService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
 public class OrderDetailController {
+    private OrderDetailService service;
+    public OrderDetailController(){
+        this.service = service;
+    };
+
+
 
 }
