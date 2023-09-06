e0007a12fd13611e8b5a5fcc32537f50eaef3806
PARKMINJUN <junee0530@naver.com>
2023-09-06 10:31:54 +0900

    [ADD] AccountMapper, AccountService, AccountServiceImpl 재업로드

---
+++ src/main/java/com/example/springreact/service/AccountServiceImpl.java
@@ -0,0 +1,48 @@
+package com.example.springreact.service;
+
+import com.example.springreact.domain.Account;
+import com.example.springreact.domain.Storage;
+import com.example.springreact.dto.AccountDTO;
+import com.example.springreact.mapper.AccountMapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AccountServiceImpl implements AccountService{
+
+    @Autowired
+    private AccountMapper mapper;
+
+    @Override
+    public List<Account> getListAll() {
+        return mapper.getListAll();
+    }
+
+    @Override
+    public Account getListOne(int account_code) {
+        return mapper.getListOne(account_code);
+    }
+
+    @Override
+    public void insert(AccountDTO accountDTO) {
+        ModelMapper modelMapper = new ModelMapper();
+        Account account = modelMapper.map(accountDTO,Account.class);
+        System.out.println("AccountServiceImpl : " + account);
+    }
+
+    @Override
+    public int update(AccountDTO accountDTO) {
+        ModelMapper modelMapper = new ModelMapper();
+        Account account = modelMapper.map(accountDTO,Account.class);
+        System.out.println("AccountServiceImpl : " + account);
+        return mapper.update(account);
+    }
+
+    @Override
+    public int delete(int account_code) {
+        return mapper.delete(account_code);
+    }
+}
